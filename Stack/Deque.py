457379e613419d0bee98186e56eaf4b1dda9d378
sharonwu827 <sywu2014@hotmaill.lcom>
2021-08-19 14:51:42 -0700

    updated

--- Stack/Deque.py
+++ Stack/Deque.py
@@ -9,4 +9,14 @@ customQueue.append(2)
 customQueue.append(3)
 customQueue.append(4)
 print(customQueue)
+print(customQueue.popleft())
+print(customQueue)
+
+import queue as q
+customQueue = q.Queue(maxsize=3)
+print(customQueue.qsize())
+
+customQueue.put(1)
+customQueue.put(2)
+customQueue.put(3)
 
