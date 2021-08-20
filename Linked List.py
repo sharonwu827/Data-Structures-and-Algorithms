7f9120e05d604bb5f98d767860bcf44f96b844f0
sharonwu827 <sywu2014@hotmaill.lcom>
2021-08-20 12:04:05 -0700

    updated

--- Linked List.py
+++ Linked List.py
@@ -1,21 +1,20 @@
-class Node:
-    def __init__(self,value=None):
+
+
+class Node(object):
+
+    def __init__(self,value):
         self.value = value
-        self.next = None
-## create single linked list
-class Slinkedlist:
-    def __init__(self):
-        self.head=None  ## --- O(1)
-        self.tail=None  ## --- O(1)
-singlyLinkedlist = Slinkedlist()
-node1=Node(1)
-node2=Node(2)
+        self.nextnode=None
+#create nodes
+a=Node(1)
+b=Node(2)
+c=Node(3)
 
-singlyLinkedlist.head=node1
-singlyLinkedlist.head.next=node2   ## --- O(1)
-singlyLinkedlist.tail = node2
+# link those nodes together
+a.nextnode = b
+b.nextnode = c
 
 
-## insertion
-
-
+a.value # return 1
+a.nextnode # return type
+a.nextnode.value # return 2
--- Stack/Deque.py
+++ Stack/Deque.py
@@ -1,22 +1,2 @@
-
-# how to use collections.deque as a FIFO queue:
-from collections import deque
-customQueue = deque(maxlen=3)
-print(customQueue)
-
-customQueue.append(1)
-customQueue.append(2)
-customQueue.append(3)
-customQueue.append(4)
-print(customQueue)
-print(customQueue.popleft())
-print(customQueue)
-
-import queue as q
-customQueue = q.Queue(maxsize=3)
-print(customQueue.qsize())
-
-customQueue.put(1)
-customQueue.put(2)
-customQueue.put(3)
+class Deque(object):
 
