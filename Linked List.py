1c2ad1be53264463df21214f5d3f36c2a959f3ec
sharonwu827 <sywu2014@hotmaill.lcom>
2021-09-20 10:33:31 -0700

    updated

--- Linked List.py
+++ Linked List.py
@@ -1,4 +1,3 @@
-
 class ListNode(object):
     def __init__(self,value):
         self.value = value
@@ -19,3 +18,37 @@ b.nextnode = c
 a.value # return 1
 a.nextnode # return type
 a.nextnode.value # return 2
+
+
+
+
+# Deletion in Linked List
+# below is the structure of a node which is used to create a new node every time
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None #none is nothing but null
+
+# creating a class for implementing the code for deletion in a linked list
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Inserting at the end of a Linked List (append function)
+    def append(self,key):
+        h=self.head
+        if h is None:
+            new_node=Node(key)
+            self.head = new_node
+        else:
+            while h.next != None:
+                h=h.next
+            new_node = Node(key)
+            h.next=new_mode
+
+
+
+
+
+
