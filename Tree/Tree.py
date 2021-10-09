4b259e05cf2c69c2b239b9f75c9ebd4fb690ecb4
swu <sywu2014@hotmail.com>
2021-10-09 13:51:20 -0700

    Update

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -14,6 +14,30 @@ def insertLeft(root,newbranch):
         root.insert(1,[newbranch,t,[]])
     else:
         root.insert(1,[newbranch,[],[]])
-        root.insert(1, [newbranch, [], []])
     return root
 
+
+def insertRight(root,newbranch):
+    t = root.pop(2)
+    if len(t)>1:
+        root.insert(1,[newbranch,[],t])
+    else:
+        root.insert(1,[newbranch,[],[]])
+    return root
+
+def getRootVal(root):
+    return root[0]
+def setRootVal(root,newVal):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChile(root):
+    return root[2]
+
+
+r = BinaryTree(3)
+
+insertLeft(r,3)
+insertRight(r,4)
