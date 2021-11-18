df1875029ed409c1354d53baa8571ffdb43d36d5
Sharon Wu <sharonwu827@gmail.com>
2021-11-18 14:25:05 -0800

    update

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -1,15 +1,38 @@
-class Stack(object):
+## we use to linkedlist to construct the stack
 
+class Node(value):
     def __init__(self):
-        self.items=[]
-    def isEmpty(self):
-        return self.items==[]
-    def push(self,item):
-        self.items.append(item)
-    def pop(self):
-        return self.items.pop()
-    def peek(self):
-        return self.items[len(self.items)-1]
-    def size(self):
-        return len(self.items)
+        self.value = value
+        self.next = None
 
+class stack:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head= new_node
+        self.length = 1
+
+    def print_stack(self):
+        p = self.head
+        for _ in self.length:
+            print p.value
+            p = p.next
+
+    def push(self,value):
+        new_node = Node(value)
+        if self.length==0:
+            self.head = new_node
+            # self.next = None
+        else:
+            new_node.next = self.head
+            self.head =  new_node
+        self.length+=1
+        return True
+
+    def pop(self):
+
+
+
+
+
+my_stack = stack(4)
+my_stack.print_stack()
