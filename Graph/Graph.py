5d1238236bec0942a5b8654b70fef028bca07cc0
Sharon Wu <sharonwu827@gmail.com>
2021-11-23 11:19:44 -0800

    update

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -29,5 +29,17 @@ class Graph:
             return True
         return False
 
-    def remove_vertex(self, v1):
-        if v1 in self.adj_list.keys():
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+
+
+
+
+
+
+
