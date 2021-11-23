19dee95e5275e35c1e89a6314434a083e7f34a66
Sharon Wu <sharonwu827@gmail.com>
2021-11-23 11:15:47 -0800

    update

---
+++ Graph/Graph.py
@@ -0,0 +1,33 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def print_graph(self):
+        for i in self.adj_list:
+            print(i, ":", self.adj_list[i])
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    def remove_vertex(self, v1):
+        if v1 in self.adj_list.keys():
