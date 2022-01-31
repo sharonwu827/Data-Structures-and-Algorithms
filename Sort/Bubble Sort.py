0dde210c7abde0a9493bdcc0e1e66be4efe916df
Sharon Wu <sharonwu827@gmail.com>
2022-01-30 20:05:48 -0800

    update

--- Sort/Bubble Sort.py
+++ Sort/Bubble Sort.py
@@ -1,11 +1,34 @@
 
 
-def bubble_sort(my_list):
-    for i in range(len(my_list)-1,0,-1): # range(start, stop, step)
+def bubble_sort(list_):
+    for i in range(len(list_)-1,0,-1): # range(start, stop, step)
         for j in range(i):
-            if my_list[j]>my_list[j+1]:
-                temp = my_list[j]
-                my_list[j],my_list[j+1] = my_list[j+1],my_list[j]
-    return my_list
+            if list_[j]>list_[j+1]:
+                list_[j],list_[j+1] = list_[j+1],list_[j]
+    return list_
 
 print(bubble_sort([1,2,0,3,6,4]))
+
+
+def selection_sort(list_):
+    for i in range(len(list_)):
+        min_= i
+        for j,v in range(i, len(list_)-1):
+            if list_[i]<list_[j]:
+
+
+
+
+def insert_sort(list_):
+    length = len(list_)
+    for i in range(1, length):
+        pre = i - 1
+        cur = list_[i]
+        while pre>=0 and arr
+
+
+
+
+
+
+
