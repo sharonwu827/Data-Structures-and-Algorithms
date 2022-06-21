e04ca0ca6cd65246a8364f4da29f959f05390f7b
Sharon Wu <sharonwu827@gmail.com>
2022-06-20 21:40:02 -0700

    update

--- Sort/Quick Sort.py
+++ Sort/Quick Sort.py
@@ -13,17 +13,15 @@ class Solution:
                 left += 1
             # 将更大的数放入右边
             nums[right] = nums[left]
-        pivot = nums[left]
+        pivot = nums[left]  # 注意这里  不是nums[left] = pivot 而是要将pivot移到该在的index
         # 返回待比较数据最终位置
         return left
 
     def quickSort(self, nums, left, right):
-        if left<right:
+        if left < right:
             index = self.partition(nums, left, right)
             self.quickSort(nums, left, index - 1)
-            self.quickSort(nums, index+1, right)
+            self.quickSort(nums, index + 1, right)
             return nums
-
-    def topKSplit(self, nums):
 
 
