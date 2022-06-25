c9ec4e8a2950bedeb237405897cc71b25b2d36b7
Sharon Wu <sharonwu827@gmail.com>
2022-06-24 22:51:43 -0700

    update

--- Recursion/Backtracking.py
+++ Recursion/Backtracking.py
@@ -31,20 +31,43 @@ class Solution:
         return res
 
 
-# 同一树层去重
-
-#
-
-
-
-# 同一树枝去重
-
-#
+# 同一树层去重 如何判断同一树层上元素（相同的元素）是否使用过了呢？
+# used
+# 如果candidates[i] == candidates[i - 1] 并且 used[i - 1] == false
+# 就说明：前一个树枝，使用了candidates[i - 1]，也就是说同一树层使用过candidates[i - 1]
 
 
+# 40 Combination Sum II  https://leetcode.com/problems/combination-sum-ii/
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        '''
+        - 原数组有重复 所以同一树层要去重
+        - 回溯时需要从下一位取 所以需要startindex 但同一树枝可以取val相同的树
+        - 需要剪枝
+        '''
+        candidates.sort()
+        res = []
+        used = [0] * len(candidates)
+
+        def backtrack(startIndex, path):
+            if sum(path) == target:
+                res.append(path[:])
+                # return
+            # 同#39题剪枝
+            if sum(path) > target:
+                return
+            for i in range(startIndex, len(candidates)):
+                if i > 0 and used[i - 1] == 0 and candidates[i] == candidates[i - 1]:
+                    continue
+                path.append(candidates[i])
+                used[i] = 1
+                backtrack(i + 1, path)
+                used[i] = 0
+                path.pop()
+        backtrack(0, [])
+        return res
 
 
-# 什么时候需要return
 # 90. Subsets II https://leetcode.com/problems/subsets-ii/
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
@@ -62,20 +85,51 @@ class Solution:
             # 这里为什么不能return
             # 有return 直接 return [[]]
             for i in range(startIndex, len(nums)):
-                if used[i] == 0:
-                    # 说明同一树层上有两个重复的元素，不可以重复选取
-                    if i > 0 and nums[i] == nums[i - 1] and used[i - 1] == 0:
-                        continue
-                    used[i] = 1
-                    path.append(nums[i])
-                    backtrack(i + 1, path)
-                    path.pop()
-                    used[i] = 0
+                if i > 0 and used[i - 1] == 0 and nums[i] == nums[i - 1]:
+                    continue
+                used[i] = 1
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+                used[i] = 0
 
         backtrack(0, [])
         return res
 
 
+# 同一树枝去重
 
 
+
+
+
+
+## 剪值
+# 39. Combination Sum https://leetcode.com/problems/combination-sum/
+# 树枝剪枝，已经知道下一层的sum会大于target，就没有必要进入下一层递归了
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        '''
+        - 原数组没有有重复 所以同一树层不需要去重
+        - 回溯时可以取当前位置 元素可无限重复选取
+        - 需要剪枝
+        '''
+        res = []
+
+        def backtrack(startIndex, path):
+            if sum(path) == target:
+                res.append(path[:])
+                # return
+            # 已经知道下一层的sum会大于target，就没有必要进入下一层递归了
+            if sum(path) > target:
+                return
+
+            for i in range(startIndex, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path)
+                path.pop()
+
+        backtrack(0, [])
+        return res
 
