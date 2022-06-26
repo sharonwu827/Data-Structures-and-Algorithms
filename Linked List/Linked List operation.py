166e28c6760a93b9164e1562afdafe56859c174e
Sharon Wu <sharonwu827@gmail.com>
2022-06-26 12:08:41 -0700

    update

--- Linked List/Linked List operation.py
+++ Linked List/Linked List operation.py
@@ -2,6 +2,7 @@ class LinkList:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
     def reverse(self):
         pre = LinkList(None)
         cur = head
@@ -24,8 +25,27 @@ class LinkList:
         length = 0
         cur = head
         while cur:
-            length+=1
-            cur =cur.next
+            length += 1
+            cur = cur.next
         return length
 
 
+class solustion:
+    def mergedTwoLists(l1, l2):
+        dummy = ListNode(None)
+        p = dummy
+        p1 = l1
+        p2 = l2
+        while p1 and p2:
+            if p1.val <= p2.val:
+                p.next = p1
+                p1 = p1.next
+            else:
+                p.next = p2
+                p2 = p2.next
+            p = p.next
+        if p1:
+            p.next = p1
+        if p2:
+            p.next = p2
+        return dummy.next
