00cb053627c475ed846c1e4716fe8265b7dcd8d8
Sharon Wu <sharonwu827@gmail.com>
2022-07-11 22:54:27 -0700

    update

--- Trie/Implement Trie.py
+++ Trie/Implement Trie.py
@@ -1,6 +1,23 @@
 class TrieNode:
     def __init__(self):
-        self.children = [None]*26
+        self.children = [None] * 26
         # isEndOfWord is True if node represent the end of the word
         self.isEndOfWord = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNodd()
+
+    def charToIndex(self, ch):
+        '''
+        :param ch: use only 'a' through 'z' and lower case
+        :return: Converts key current character into index
+        '''
+        return ord(ch) - ord('a')
+
+    def insert(self, key):
+
+
+
 
