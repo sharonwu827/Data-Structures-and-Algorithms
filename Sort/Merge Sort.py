42464ef3dedfc56d9bbac1ffdfdc739f50dbf997
Sharon Wu <sharonwu827@gmail.com>
2022-08-03 22:29:50 -0700

    update

--- Sort/Merge Sort.py
+++ Sort/Merge Sort.py
@@ -1,31 +1,36 @@
-def merge_sort(arr):
+def mergesort(self,seq):
+		"""归并排序"""
+		if len(seq) <= 1:
+			return seq
+		mid = len(seq) // 2  # 将列表分成更小的两个列表
 
-    n = len(arr)
-    if n <= 1:
-        return arr
-    mid = n // 2
+		# 分别对左右两个列表进行处理，分别返回两个排序好的列表
+		left = self.mergesort(seq[:mid])
+		right = self.mergesort(seq[mid:])
 
-    arrA = merge_sort(arr[:mid])
-    arrB = merge_sort(arr[mid:])
-    # 直到这里都是分割
+		# 对排序好的两个列表合并，产生一个新的排序好的列表
+		return self.merge(left, right)
 
-    # 接下来才是比较合并
-    arrA_index, arrB_index = 0, 0
-    sorted_array = []
-    while arrA_index < len(arrA) and arrB_index < len(arrB):
-        if arrA[arrA_index] < arrB[arrB_index]:
-            sorted_array.append(arrA[arrA_index])
-            arrA_index += 1
-        else:
-            sorted_array.append(arrB[arrB_index])
-            arrB_index += 1
-    sorted_array += arrA[arrA_index:]
-    sorted_array += arrB[arrB_index:]
-
-    return sorted_array
+	def merge(self, left, right):
+		"""合并两个已排序好的列表，产生一个新的已排序好的列表"""
+		result = []  # 新的已排序好的列表
+		i = 0  # 下标
+		j = 0
+		# 对两个列表中的元素 两两对比。
+		# 将最小的元素，放到result中，并对当前列表下标加1
+		while i < len(left) and j < len(right):
+			if left[i] <= right[j]:
+				result.append(left[i])
+				i += 1
+			else:
+				result.append(right[j])
+				j += 1
+		result += left[i:]
+		result += right[j:]
+		return result
 
 arr = [ 12, 34, 54, 2, 3, -1, 0, -16]
-print(merge_sort(arr))
+print(mergesort(arr))
 
 # result
 # [-16, -1, 0, 2, 3, 12, 34, 54]
